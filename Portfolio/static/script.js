658886f6544f6cc05c912e351cf111a7a756546f
daniel <daniel@example.com>
2021-06-05 07:35:29 +0100

    Job panel changes when button is clicked and Particle js moved to script.js

---
+++ Portfolio/static/script.js
@@ -0,0 +1,120 @@
+const canvas = document.getElementById('canvas-particle');
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight
+
+let particlesArray;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+window.addEventListener('mousemove', (e)=>{
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+class Particle {
+    constructor(x, y, directionX, directionY, size, color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+    }
+    update() {
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;  
+        let distance = Math.sqrt(dx*dx + dy*dy)
+        if (distance < mouse.radius + this.size) {
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10){
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x >  this.size * 10){
+                this.x -= 10;
+                this.directionX = -this.directionX;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10){
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y >  this.size * 10){
+                this.y -= 10;
+                this.directionY = -this.directionY;
+            }
+        }
+        this.x += this.directionX;
+        this.y += this.directionY;
+        this.draw()
+    }
+}
+
+function init(){
+    particlesArray = []
+    let numberOfParticles = (canvas.height * canvas.width) / 15000;
+    for (let i=0; i < numberOfParticles; i++) {
+        let size = (Math.random() *5) + 1
+        let x = (Math.random() * ((window.innerWidth - size * 2) - (size * 2))  + size * 2);
+        let y = (Math.random() * ((window.innerHeight - size * 2) - (size * 2))  + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = "#FFF";
+
+        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function connect() {
+    let opacityValue = 1;
+    for (let a=0; a < particlesArray.length; a++){
+        for (let b=0; b < particlesArray.length; b++){
+            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y))
+            if (distance < (canvas.width / 7) *  (canvas.height / 7)) {
+                opacityValue = 1 - (distance/20000);
+                ctx.strokeStyle ='rgba(236, 236, 236,' +  opacityValue +')';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y)
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+function animate(){
+    requestAnimationFrame(animate)
+    ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+    for (let i=0; i < particlesArray.length; i++){
+        particlesArray[i].update()
+    }
+    connect()
+}
+
+window.addEventListener('resize', (e)=> {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    mouse.radius = ((canvas.height/80) * (canvas.height/80))
+    init()
+})
+window.addEventListener('mouseout', (e)=> {
+    mouse.x = undefined;
+    mouse.y = undefined;
+})
+init();
+animate();
