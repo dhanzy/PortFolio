896c8de221e64f749c22cf51f9fdebe7760c0401
daniel <holuwadanzy@gmail.com>
2021-10-14 08:37:39 +0100

    Removed send_from_directory and put send_file in main.routes

--- Portfolio/main/routes.py
+++ Portfolio/main/routes.py
@@ -7,10 +7,11 @@ from flask import render_template, Blueprint, send_file, send_from_directory
 main = Blueprint('main', __name__)
 
 
-
 @main.route('/<filename>')
 def get_static_files(filename):
-    return send_from_directory('static', filename=filename, as_attachment=True)
+    path = os.path.join('static',filename)
+    return send_file(path, as_attachment=True)
+    # return send_from_directory(path, filename=filename, as_attachment=True)
 
 
 @main.route('/')
