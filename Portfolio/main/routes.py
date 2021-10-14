05e2a11adffee60e2dc02e0140f011a791429e4f
daniel <holuwadanzy@gmail.com>
2021-10-14 08:23:56 +0100

    Changed Image added an ico and svg union image and updated my resume

--- Portfolio/main/routes.py
+++ Portfolio/main/routes.py
@@ -7,21 +7,10 @@ from flask import render_template, Blueprint, send_file, send_from_directory
 main = Blueprint('main', __name__)
 
 
-@main.route('/manifest.webmanifest')
-def webmanifest():
-    path = os.path.join('static','manifest.webmanifest')
-    return send_file(path, as_attachment=True)
 
-
-
-@main.route('/resume.pdf')
-def resume():
-    # path = os.path.join('Portfolio','static')
-    # if not os.path.exists(os.path.join(path, filename)):
-    #     return os.getcwd()
-    os.path.join('static','resume.pdf')
-    return send_file(path, as_attachment=True)
-    # return send_from_directory(path, filename=filename, as_attachment=True)
+@main.route('/<filename>')
+def get_static_files(filename):
+    return send_from_directory('static', filename=filename, as_attachment=True)
 
 
 @main.route('/')
