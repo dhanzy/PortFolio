fb3c7f4ccffdeb433c2d49cda92dce923fe13d4f
dhanzy <holuwadanzy@gmail.com>
2022-07-12 16:55:10 -0500

    Made branch master

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-
 class Config(object):
-    SECRET_KEY = 'mysecretkey'
+    SECRET_KEY = "mysecretkey"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Mail Configuration
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = "smtp.gmail.com"
 
 
 class Debug(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     DEBUG = True
 
+
 class Production(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:password@localhost/daniel'
+    SQLALCHEMY_DATABASE_URI = "postgresql://admin:password@localhost/daniel"
     DEBUG = False
 
 
-config_dict = {'Debug': Debug, 'Production': Production}
+config_dict = {"Debug": Debug, "Production": Production}
